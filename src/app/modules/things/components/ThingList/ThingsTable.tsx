19938a405eb02c26229d7a727287241940e15cb5
Viswanadh-sastry <viswa836@gmail.com>
2025-05-15 23:43:56 +0530

    Revert "Added sorting logic for ThingsTable"

--- src/app/modules/things/components/ThingList/ThingsTable.tsx
+++ src/app/modules/things/components/ThingList/ThingsTable.tsx
@@ -10,16 +10,22 @@ import { Thing } from "../../api/_models";
 import { AddThing } from "../AddEditThing/AddThing";
 import { ImportThings } from "../AddEditThing/ImportThings/ImportThings";
 import { ThingsListHeader } from "./ThingsListHeader";
+import { CustomHeaderColumn } from "./columns/CustomHeaderColumn";
 import { CustomRow } from "./columns/CustomRow";
 import { thingsColumns } from "./columns/_columns";
 import { ThingsListLoading } from "./pagination/ThingsListLoading";
 import { ThingsListPagination } from "./pagination/ThingsListPagination";
-import { useTableSort } from "../../../../hooks/useTableSort";
 
 const ThingsTable = () => {
   const [showAddThing, setShowAddThing] = useState(false);
   const [importModal, setImportModal] = useState(false);
-
+  const filterChannel = {
+    limit: 10,
+    offset: 0,
+    name: "",
+    metadata: "",
+    status: "enabled",
+  };
   const [filterThing, setFilterThing] = useState({
     limit: 10,
     offset: 0,
@@ -29,13 +35,8 @@ const ThingsTable = () => {
     status: "enabled",
   });
 
-  const filterChannel = {
-    limit: 10,
-    offset: 0,
-    name: "",
-    metadata: "",
-    status: "enabled",
-  };
+  const onShowImportThing = () => setImportModal(true);
+  const onCloseImportThing = () => setImportModal(false);
 
   const thingListQuery = useQuery({
     queryKey: [`thingList`, filterThing],
@@ -45,9 +46,7 @@ const ThingsTable = () => {
           const things = await Promise.all(
             response.things.map(async (thing: any) => {
               try {
-                const channel = await getThingChannelList(thing.id, filterChannel).catch((error) =>
-                  toast.error(error?.response?.data?.message || "Something went wrong")
-                );
+                const channel = await getThingChannelList(thing.id, filterChannel).catch((error) => toast.error(error?.response?.data?.message || "Something went wrong"));
                 const historyData = await Promise.all(
                   channel.groups.map(async (group: any) => {
                     try {
@@ -58,9 +57,7 @@ const ThingsTable = () => {
                         publisher: thing.id,
                         status: "enabled",
                       };
-                      const history = await getHistoryList(group.id, filterHistory).catch((error) =>
-                        toast.error(error?.response?.data?.message || "Something went wrong")
-                      );
+                      const history = await getHistoryList(group.id, filterHistory).catch((error) => toast.error(error?.response?.data?.message || "Something went wrong"));
                       return history;
                     } catch (error) {
                       return [];
@@ -70,11 +67,15 @@ const ThingsTable = () => {
 
                 const flatHistory: any = historyData.flat().sort((a: any, b: any) => a.time - b.time);
 
+                // Convert current time to Unix timestamp
                 const now = Number(String(new Date().getTime()).slice(0, 10));
+
+                // Calculate activity status
                 let activity = "inactive";
 
                 if (thing.metadata?.Update_Frequency) {
                   const updateFrequency = parseInt(thing.metadata.Update_Frequency);
+
                   if (flatHistory.length > 0 && flatHistory[0].messages?.length > 0) {
                     const firstRecordTime = Number(String(flatHistory[0].messages[0].time).slice(0, 10));
                     const timeDifference = now - firstRecordTime;
@@ -88,12 +89,7 @@ const ThingsTable = () => {
                   ...thing,
                   isConnected: channel.total > 0,
                   activity,
-                  lastSeenMsg:
-                    flatHistory.length > 0 &&
-                    flatHistory[0].messages?.length > 0 &&
-                    flatHistory[0].messages[0].time
-                      ? flatHistory[0].messages[0].time
-                      : null,
+                  lastSeenMsg: flatHistory.length > 0 && flatHistory[0].messages?.length > 0 && flatHistory[0].messages[0].time ? flatHistory[0].messages[0].time : null,
                 };
               } catch (error) {
                 return {
@@ -112,42 +108,35 @@ const ThingsTable = () => {
   });
 
   const isLoading = thingListQuery.isLoading;
-  const rawData = useMemo(() => thingListQuery.data?.things || [], [thingListQuery.data]);
-  const { sortConfig, sortedData, handleSort } = useTableSort<Thing>(rawData);
-
+  const data = useMemo(() => thingListQuery.data?.things || [], [thingListQuery.data]);
   const columns = useMemo(() => thingsColumns, []);
   const { getTableProps, getTableBodyProps, headers, rows, prepareRow } = useTable({
     columns,
-    data: sortedData,
+    data,
   });
+
+  const onShowAddThing = () => {
+    setShowAddThing(true);
+  };
+
+  const onCloseAddThing = () => {
+    setShowAddThing(false);
+  };
+
+  const onGetThingList = () => {
+    thingListQuery.refetch();
+  };
 
   return (
     <KTCard>
-      <ThingsListHeader
-        onShowAddThing={() => setShowAddThing(true)}
-        onShowImportThing={() => setImportModal(true)}
-        setFilterThing={setFilterThing}
-        filterThing={filterThing}
-      />
+      <ThingsListHeader onShowAddThing={onShowAddThing} onShowImportThing={onShowImportThing} setFilterThing={setFilterThing} filterThing={filterThing} />
       <KTCardBody className="py-4">
         <div className="table-responsive">
-          <table
-            id="kt_table_things"
-            className="table align-middle table-row-dashed fs-6 dataTable no-footer"
-            {...getTableProps()}
-          >
+          <table id="kt_table_things" className="table align-middle table-row-dashed fs-6 dataTable no-footer" {...getTableProps()}>
             <thead>
               <tr className="text-start text-muted fw-bolder fs-7 text-uppercase gs-0">
                 {headers.map((column: ColumnInstance<Thing>) => (
-                  <th
-                    key={column.id}
-                    style={{ cursor: "pointer" }}
-                    onClick={() => handleSort(column.id as keyof Thing)}
-                  >
-                    {column.render("Header")}
-                    {sortConfig?.key === column.id &&
-                      (sortConfig.direction === "asc" ? " ▲" : " ▼")}
-                  </th>
+                  <CustomHeaderColumn key={column.id} column={column} />
                 ))}
               </tr>
             </thead>
@@ -160,9 +149,7 @@ const ThingsTable = () => {
               ) : (
                 <tr>
                   <td colSpan={7}>
-                    <div className="d-flex text-center w-100 align-content-center justify-content-center">
-                      No matching records found
-                    </div>
+                    <div className="d-flex text-center w-100 align-content-center justify-content-center">No matching records found</div>
                   </td>
                 </tr>
               )}
@@ -170,19 +157,8 @@ const ThingsTable = () => {
           </table>
         </div>
         <ThingsListPagination filterThing={filterThing} setFilterThing={setFilterThing} />
-        {showAddThing && (
-          <AddThing
-            onCloseAddThing={() => setShowAddThing(false)}
-            onGetThingList={() => thingListQuery.refetch()}
-          />
-        )}
-        {importModal && (
-          <ImportThings
-            onCloseImportThing={() => setImportModal(false)}
-            onGetThingList={() => thingListQuery.refetch()}
-            onShowImportThing={importModal} 
-          />
-        )}
+        {showAddThing && <AddThing onCloseAddThing={onCloseAddThing} onGetThingList={onGetThingList} />}
+        {importModal && <ImportThings onShowImportThing={importModal} onCloseImportThing={onCloseImportThing} onGetThingList={onGetThingList} />}
         {isLoading && <ThingsListLoading />}
       </KTCardBody>
     </KTCard>
